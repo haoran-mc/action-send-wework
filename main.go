d3322e3fe209be1cbd502e1131010c71f3018371
haoran <haoran.mc@outlook.com>
2025-06-20 00:11:41 +0800

    read the shared directory

--- main.go
+++ main.go
@@ -1,26 +1,62 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
+
 	"github.com/haoran-mc/action-send-wework/model"
 	googledrive "github.com/haoran-mc/action-send-wework/repository/google-drive"
 	"github.com/haoran-mc/golib/pkg/env"
 	"github.com/haoran-mc/golib/pkg/log"
 )
 
+var readfiles = []string{"ideas.txt", "memorial-days.txt", "daily-reminder.txt"}
+
+var fileRandomProbabilityMap = map[string]float32{
+	"ideas.txt":          1, // plain text
+	"memorial-days.txt":  0, // formatting text
+	"daily-reminder.txt": 1, // plain text
+}
+
 func main() {
+	var err error
+	err = googledrive.InitDriveService()
+	if err != nil {
+		// TODO 重试
+	}
+
+	dirID := env.GetEnv("DIR_ID", "")
+	files, err := googledrive.ReadDir(dirID)
+	if err != nil {
+		// TODO 重试
+	}
+
+	var sendStr string
+	for _, filename := range readfiles {
+		for _, f := range files {
+			if f.Name != filename {
+				continue
+			}
+			rf := rand.Float32()
+			log.Info(fmt.Sprintf("%s\t%s\t%s: %b\n", f.Id, f.Name, f.Md5Checksum, rf))
+			if rf > fileRandomProbabilityMap[filename] {
+				continue
+			}
+			fileContent, err := googledrive.ReadFile(f.Id)
+			if err != nil {
+				log.Error("fail to read file", err)
+				sendStr += fmt.Sprintf("read f.Name failed, error: %v\n", err)
+				continue
+			}
+			sendStr += string(fileContent)
+		}
+	}
+	log.Info(sendStr)
+
 	robotKey := env.GetEnv("BOT_KEY", "")
 	robot := model.Robot{Key: robotKey}
 
-	credentialsJSON := env.GetEnv("GDRIVE_CREDENTIALS", ``)
-	fileID := env.GetEnv("FILE_ID", "")
-
-	fileContent, err := googledrive.ReadFile(credentialsJSON, fileID)
-	if err != nil {
-		// 重试
-	}
-
-	// 文本
-	res, err := robot.SendText(string(fileContent))
+	res, err := robot.SendText(sendStr)
 	if err != nil {
 		log.Error("fail to send text", err)
 	} else if res != nil && res.ErrorCode != 0 {
--- repository/google-drive/read.go
+++ repository/google-drive/read.go
@@ -2,44 +2,47 @@ package googledrive
 
 import (
 	"context"
+	"fmt"
 	"io"
 
+	"github.com/haoran-mc/golib/pkg/env"
 	"github.com/haoran-mc/golib/pkg/log"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
 )
 
-func ReadFile(credentialsJSON, fileID string) (fileContent []byte, err error) {
-	ctx := context.Background()
+var driveService *drive.Service
 
-	// 从环境变量获取凭证和配置
+func InitDriveService() (err error) {
+	credentialsJSON := env.GetEnv("GDRIVE_CREDENTIALS", "")
 	if credentialsJSON == "" {
 		log.Error("empty environment variable GDRIVE_CREDENTIALS")
-		return nil, err
+		return fmt.Errorf("credentials JSON is empty")
 	}
 
-	// Google Drive 中文件的 ID
-	if fileID == "" {
-		log.Error("empty environment variable FILE_ID")
-		return nil, err
-	}
+	ctx := context.Background()
 
 	// 使用服务账号凭证创建授权的客户端
 	creds, err := google.CredentialsFromJSON(ctx, []byte(credentialsJSON), drive.DriveReadonlyScope)
 	if err != nil {
-		log.Error("unable to create client from credentials", err)
-		return nil, err
+		log.Error("unable to create client from credentials" + err.Error())
+		return
 	}
-
-	// 创建一个经过授权的 Drive 服务
-	driveService, err := drive.NewService(ctx, option.WithCredentials(creds))
+	// 创建经过授权的 Drive 服务
+	driveService, err = drive.NewService(ctx, option.WithCredentials(creds))
 	if err != nil {
-		log.Error("unable to create Drive service", err)
+		log.Error("fail to create drive service", err)
+	}
+	return
+}
+
+func ReadFile(fileID string) (fileContent []byte, err error) {
+	if fileID == "" {
+		log.Error("FILE_ID not provided")
 		return nil, err
 	}
 
-	// 下载文件内容
 	resp, err := driveService.Files.Get(fileID).Download()
 	if err != nil {
 		log.Error("unable to download file", err)
@@ -52,4 +55,40 @@ func ReadFile(credentialsJSON, fileID string) (fileContent []byte, err error) {
 		log.Error("fail to read file content", err)
 	}
 	return
+}
+
+func ReadDir(dirID string) ([]*drive.File, error) {
+	if dirID == "" {
+		log.Error("empty environment variable DIR_ID")
+		return nil, fmt.Errorf("directory ID is empty")
+	}
+
+	// 'trashed = false' 不列出回收站中文件
+	query := fmt.Sprintf("'%s' in parents and trashed = false", dirID)
+
+	var allFiles []*drive.File
+	pageToken := ""
+
+	// drive api 使用分页来返回结果，循环获取所有页面
+	for {
+		req := driveService.Files.List().
+			Q(query).
+			Fields("nextPageToken, files(*)"). // drive api 默认返回 id, name, mimeType，使用 "*" 请求所有可用字段
+			PageToken(pageToken)
+
+		r, err := req.Do()
+		if err != nil {
+			log.Error("unable to retrieve files", err)
+			return nil, err
+		}
+
+		allFiles = append(allFiles, r.Files...)
+
+		// 如果有下一页，则更新 pageToken 继续
+		pageToken = r.NextPageToken
+		if pageToken == "" {
+			break
+		}
+	}
+	return allFiles, nil
 }
